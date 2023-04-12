3e12cea291182163c550d01758693fbbd20dc234
Giorgi Patsia <gio.patsia@gmail.com>
2023-04-12 10:08:59 +0200

    last test

--- app.js
+++ app.js
@@ -1,22 +1,17 @@
 const slide = document.querySelector('input[type="range"]');
 let money = document.querySelector(".moneyMonth h3");
-console.log(typeof money.textContent);
 const pageViews = document.querySelector(".middleDiv p");
-console.log(pageViews.textContent);
 const toggle = document.querySelector('input[type="checkbox"]');
 
 let values = [0, 8, 12, 16, 24, 36];
 let pageViewers = [0, 10, 50, 100, 500, 1000, 1];
-let amount = Number(0);
+let amount = Number(16);
 let earlyBilling;
 
 const rangeChange = slide.addEventListener("input", function () {
   const slideValue = parseInt(slide.value);
-  console.log(slideValue);
   amount = `${values[slideValue]}`;
-  console.log(amount);
   money.textContent = `$${amount}`;
-
   if (amount === "36") {
     pageViewers[5] = "1M";
     pageViews.textContent = `${pageViewers[5]} pageviews`;
@@ -28,15 +23,30 @@ const rangeChange = slide.addEventListener("input", function () {
   } else {
     toggle.disabled = false;
   }
-});
 
-toggle.addEventListener("change", function () {
   if (toggle.checked) {
-    console.log(amount * 12 - 0.25 * amount * 12);
     earlyBilling = amount * 12 - 0.25 * amount * 12;
     money.textContent = `$${earlyBilling}`;
   } else {
     money.textContent = `$${amount}`;
-    console.log("off");
   }
+});
+
+toggle.addEventListener("change", function () {
+  if (toggle.checked) {
+    earlyBilling = amount * 12 - 0.25 * amount * 12;
+    money.textContent = `$${earlyBilling}`;
+  } else {
+    money.textContent = `$${amount}`;
+  }
+});
+
+const slideColorChange = (slide.oninput = function () {
+  const value = ((this.value - this.min) / (this.max - this.min)) * 100;
+  this.style.background =
+    "linear-gradient(to right, #A4F3EB 0%, #ECF0FB " +
+    value +
+    "%, #fff " +
+    value +
+    "%, white 100%)";
 });
